846307b490de619829584d82ec3d244f3a6a7349
bhiaibogf <bhiaibogf@outlook.com>
2019-07-15 18:55:12 +0800

    comments & TODO

--- knn/knn.py
+++ knn/knn.py
@@ -8,15 +8,20 @@ sys.path.append('../window')
 from window import window
 
 
+# TODO 正规化数据
 def calc_eucl(x, y):
     return sqrt(np.sum([(xx - yy) ** 2 for xx, yy in zip(x, y)]))
 
 
+# TODO 其他的距离计算方式
+
+
+# TODO 不同的k
 # kNN
-def knn(teachers, tests, cal_func, k=3):
+def knn(teachers, tests, cal_dis, k=3):
     # 记录各种行为在周围k个元素中出现了几次
     result = {}
-    tmp = [[cal_func(teacher[:-1], test[:-1]), teacher[-1]] for test in tests for teacher in teachers]
+    tmp = [[cal_dis(teacher[:-1], test[:-1]), teacher[-1]] for test in tests for teacher in teachers]
     # 排序选出最近的k个元素
     for kth in sorted(tmp, key=lambda x: x[0])[:k]:
         # print(kth)
@@ -32,18 +37,22 @@ def is_equal(ans, pre):
 
 
 if __name__ == "__main__":
+    # TODO 数据去噪
     # 训练集
     teacher = []
     for i in [1, 2, 3, 4, 5, 6, 8, 10, 11]:
         print(i)
-        teacher = [d for d in window("../data/teacher/" + str(i) + "oo.csv", i)]
+        teacher.extend([d for d in window("../data/teacher/" + str(i) + "oo.csv", i)])
 
     # 测试集
-    test = [d for d in window("../data/test/1test.csv")]
+    test = []
+    for i in [1, 2, 3, 4, 5, 6, 8, 10, 11]:
+        print(i)
+        test.extend([d for d in window("../data/test/" + str(i) + "test.csv", i)])
     # print(test)
+    # TODO 混淆矩阵
     # 测试结果
     cor = reduce(lambda a, b: a + b, [is_equal(i[-1], knn(teacher, i[:-1], calc_eucl)) for i in test])
+    # TODO 其他表征结果的参数
     # 输出各种结果
     print("Accuracy : " + str(cor / len(test) * 100) + " %")
-    print(test[0][-1])
-    print(knn(teacher, test[0][:-1], calc_eucl))
--- window/window.py
+++ window/window.py
@@ -2,22 +2,30 @@ import numpy as np
 
 
 # 将文件中的数据分成不同的窗口
-def window(filename, type, width=100, step=50):
-    # 特征值
+# width是窗口宽度
+# 1-step/width是窗口重叠度
+# TODO 调整以上两项
+
+def window(filename, type, width=200, step=75):
+    # 特征值(平均值，方差)(使用list储存各个传感器参数) + 实际的运动类型
     argvs = []
+    # TODO 更多类型的特征值
     with open(filename, mode="r") as f:
-        # 读入一行数据并将其分割后转化为一个float的list
+        # 读入一行数据并将其分割后转化为一个float的list(共16个传感器参数)
         lines = [list(map(lambda x: float(x), l.split(",")[4:20]))
                  for l in f.readlines()]
-    for wl in [lines[i:i + width] for i in range(0, len(lines) - 1, step)]:
+    print(lines)
+    for win in [lines[i:i + width] for i in range(0, len(lines) - 1, step)]:
         argv = []
-        argv.append(list(np.average(np.array(wl), axis=0)))
-        argv.append(list(np.var(np.array(wl), axis=0)))
-        argv.append(np.median(np.array(type)))
+        argv.append(list(np.average(np.array(win), axis=0)))
+        argv.append(list(np.var(np.array(win), axis=0)))
+        argv.append(type)
         # print(argv)
         argvs.append(argv)
     return argvs
 
 
+# just for test
 if __name__ == "__main__":
-    argvs = window("../data/teacher/1oo.csv", 1)
+    argvs = window("../data/test/11test.csv", 1)
+    print(argvs)
